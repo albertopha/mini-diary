cde6ef1b1d433df40329690273a4dddfc5d3d71a
Albert-Weon-Jun-Oh <Albert.Weon.Jun.Oh@ibm.com>
2019-04-11 16:52:01 -0400

    init commit

---
+++ src/main/java/com/diaries/miniDiary/MiniDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.diaries.miniDiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniDiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diaries/miniDiary/MiniDiaryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.diaries.miniDiary;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MiniDiaryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
